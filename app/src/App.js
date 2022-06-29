cb427fc716c8f327984d5a8f2c0995f588282983
Andre Junior <junior-andre50@hotmail.com>
2022-06-29 15:52:56 -0300

    update

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Content">
       <p>Naruto </p>
       <button> Jutsu</button>
       <img src={logo} alt="Logo"/>
